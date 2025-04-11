15825ff3863d956fcffc5dccd9a4beacd0790bc0
krish-wappnet <krish.wappnet@gmail.com>
2025-04-11 12:16:07 +0530

    Final touches

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,65 +1,35 @@
-// src/components/Navbar.tsx
 import React from 'react';
+import logo from '../assets/logo.png'; // adjust path if needed
 
 const Navbar: React.FC = () => {
   return (
     <div className="fixed top-0 left-0 w-full flex items-center justify-between px-12 py-4 bg-black z-50">
-      {/* Logo */}
-      <div className="text-2xl font-bold tracking-widest text-white">
-        GERÍCHT
+      {/* Logo Image */}
+      <div>
+        <img src={logo} alt="Gericht Logo" className="h-10 object-contain" />
       </div>
 
       {/* Navigation - centered on screen */}
       <nav className="absolute left-1/2 transform -translate-x-1/2">
-        <ul className="flex space-x-8 text-sm">
-          <li>
-            <a
-              href="#"
-              className="relative text-white hover:text-yellow-600 transition-colors duration-300 after:content-[''] after:absolute after:h-0.5 after:w-0 after:left-0 after:-bottom-1 after:bg-yellow-600 hover:after:w-full after:transition-all after:duration-300"
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="relative text-white hover:text-yellow-600 transition-colors duration-300 after:content-[''] after:absolute after:h-0.5 after:w-0 after:left-0 after:-bottom-1 after:bg-yellow-600 hover:after:w-full after:transition-all after:duration-300"
-            >
-              Pages
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="relative text-white hover:text-yellow-600 transition-colors duration-300 after:content-[''] after:absolute after:h-0.5 after:w-0 after:left-0 after:-bottom-1 after:bg-yellow-600 hover:after:w-full after:transition-all after:duration-300"
-            >
-              Contact Us
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="relative text-white hover:text-yellow-600 transition-colors duration-300 after:content-[''] after:absolute after:h-0.5 after:w-0 after:left-0 after:-bottom-1 after:bg-yellow-600 hover:after:w-full after:transition-all after:duration-300"
-            >
-              Blog
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="relative text-white hover:text-yellow-600 transition-colors duration-300 after:content-[''] after:absolute after:h-0.5 after:w-0 after:left-0 after:-bottom-1 after:bg-yellow-600 hover:after:w-full after:transition-all after:duration-300"
-            >
-              Landing
-            </a>
-          </li>
+        <ul className="flex space-x-10 text-base">
+          {['Home', 'Pages', 'Contact Us', 'Blog', 'Landing'].map((item) => (
+            <li key={item}>
+              <a
+                href="#"
+                className="relative text-white hover:text-yellow-600 transition-colors duration-300 after:content-[''] after:absolute after:h-0.5 after:w-0 after:left-0 after:-bottom-1 after:bg-yellow-600 hover:after:w-full after:transition-all after:duration-300"
+              >
+                {item}
+              </a>
+            </li>
+          ))}
         </ul>
       </nav>
 
       {/* Auth and Booking with partition */}
-      <div className="flex items-center">
+      <div className="flex items-center text-base">
         <a
           href="#"
-          className="relative text-sm text-white hover:text-yellow-600 transition-colors duration-300 after:content-[''] after:absolute after:h-0.5 after:w-0 after:left-0 after:-bottom-1 after:bg-yellow-600 hover:after:w-full after:transition-all after:duration-300"
+          className="relative text-white hover:text-yellow-600 transition-colors duration-300 after:content-[''] after:absolute after:h-0.5 after:w-0 after:left-0 after:-bottom-1 after:bg-yellow-600 hover:after:w-full after:transition-all after:duration-300"
         >
           Log In / Registration
         </a>
@@ -68,7 +38,7 @@ const Navbar: React.FC = () => {
 
         <a
           href="#"
-          className="relative text-sm text-white hover:text-yellow-600 transition-colors duration-300 after:content-[''] after:absolute after:h-0.5 after:w-0 after:left-0 after:-bottom-1 after:bg-yellow-600 hover:after:w-full after:transition-all after:duration-300"
+          className="relative text-white hover:text-yellow-600 transition-colors duration-300 after:content-[''] after:absolute after:h-0.5 after:w-0 after:left-0 after:-bottom-1 after:bg-yellow-600 hover:after:w-full after:transition-all after:duration-300"
         >
           Book Table
         </a>
